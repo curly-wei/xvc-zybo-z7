13221246ca551ed7245a5ef7f8e192e8b242e701
curlywei <curlywei@gmail.com>
2024-05-18 20:50:24 +0800

    apply clone func

--- src/utilities/makefile/clone_remote_repo.mk
+++ src/utilities/makefile/clone_remote_repo.mk
@@ -1,10 +1,25 @@
-#Color renderning Green
+###
+# @file: clone_remote_repo.mk
+# @brief: Contain function for clone/pull from remote repo.
+# @author: dewei
+# @date: 2024-5-18
+# @version 0.0.1 
+###
+
+
+###
+# @brief: Check whether remote repo. eixis and newest in local.
+#
+# @param ${1}: URL of remote repo
+# @param ${2}: The branch tag of remote repo
+# @param ${3}: The path for download to local
+###
 define CloneRemoteRepo
-	@if [ ! -d ${3} ]; then \
+	if [ ! -d ${3} ]; then \
 		echo "Remote repository doesn't exist in local, cloning...";\
 		git clone \
 		--depth=1 \
-		--branch=${2} \
+		-b ${2} --\
 		${1} ${3}; \
 	else \
 		echo "Remote repository exist in local, check newest..."; \
--- src/utilities/makefile/color_printer.mk
+++ src/utilities/makefile/color_printer.mk
@@ -1,13 +1,29 @@
-#Color renderning Green
+###
+# @file: Color renderning from the input string.
+# @brief: Contain function for clone/pull from remote repo.
+# @author: dewei
+# @date: 2024-5-18
+# @version 0.0.1 
+###
+
+
+
+###
+# @brief: Color renderning Green(info) and output to stdout
+# @param ${1}: Input string
+###
 define InfoPrint 
 	@$(eval kColorGreen := '\e[1;32m')
 	@$(eval kColorEnd := '\e[0m')
 	@echo -e $(kColorGreen)"[UserINFO]: "${1}$(kColorEnd)
 endef
 
-#Color renderning Red
+###
+# @brief: Color renderning red(error) and output to stderr
+# @param ${1}: Input string
+###
 define ErrorPrint 
 	@$(eval kColorRed := '\e[1;31m')
 	@$(eval kColorEnd := '\e[0m')
-	@echo -e $(kColorGreen)"[UserINFO]: "${1}$(kColorEnd)
+	@echo -e $(kColorGreen)"[Error]: "${1}$(kColorEnd) >&2
 endef
